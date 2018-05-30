11865976f1660872be7909012e440dc0324ba617
Annie Shin and Nick Brown <annie@>
2018-05-30 11:18:19 -0700

    add rest of Js terms to list

---
+++ README.md
@@ -0,0 +1,19 @@
+JavaScript (JS)
+Operators
+Variables
+Variable naming conventions
+Functions
+Methods
+Arguments
+Parameters
+Return
+Chaining methods
+Strings
+Booleans
+Undefined
+NaN
+Escape
+Alert
+Comments
+jQuery
+Attributes
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,65 @@
 $(document).ready(function() {
-   $(".JavaScript").click(function() {
-    $(".def-js-style").toggle();
-    $(".def-js-hidden").show();
-    
- });
+  $(".JavaScript").click(function() {
+    $(".def-js").toggle();
+  });
+
+  $(".Operators").click(function() {
+    $(".def-op").toggle();
+  });
+
+  $(".Variables").click(function() {
+    $(".def-var").toggle();
+  });
+
+  $(".vnc").click(function() {
+    $(".def-vnc").toggle();
+  });
+
+  $(".Functions").click(function() {
+    $(".def-fun").toggle();
+  });
+
+  $(".Methods").click(function() {
+    $(".def-meth").toggle();
+  });
+  $(".Arguments").click(function() {
+    $(".def-Arguments").toggle();
+  });
+  $(".Parameters").click(function() {
+    $(".def-Parameters").toggle();
+  });
+  $(".Return").click(function() {
+    $(".def-Return").toggle();
+  });
+  $(".Chaining-methods").click(function() {
+    $(".def-Chaining-methods").toggle();
+  });
+  $(".Strings").click(function() {
+    $(".def-Strings").toggle();
+  });
+  $(".Booleans").click(function() {
+    $(".def-Booleans").toggle();
+  });
+  $(".Undefined").click(function() {
+    $(".def-Undefined").toggle();
+  });
+  $(".NaN").click(function() {
+    $(".def-NaN").toggle();
+  });
+  $(".Escape").click(function() {
+    $(".def-Escape").toggle();
+  });
+  $(".Alert").click(function() {
+    $(".def-Alert").toggle();
+  });
+  $(".Comments").click(function() {
+    $(".def-Comments").toggle();
+  });
+  $(".jQuery").click(function() {
+    $(".def-jQuery").toggle();
+  });
+  $(".Attributes").click(function() {
+    $(".def-Attributes").toggle();
+  });
+
 });
